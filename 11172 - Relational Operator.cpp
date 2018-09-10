6e336fab691a8eba3e97b0877ced027ae9b761fe
Abdul Monim <43042797+Monim752@users.noreply.github.com>
2018-09-10 22:04:17 +0600

    Add files via upload

---
+++ 11172 - Relational Operator.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d%d",&a,&b);
+        if(a>b)
+            printf(">\n");
+        else if(a==b)
+            printf("=\n");
+        else
+            printf("<\n");
+    }
+    return 0;
+}
